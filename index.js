a97b9a265a53685da5b2655f5dac7b815f297df7
dinesh chandrasekaren <dineshchandrasekaren@gmail.com>
2021-07-01 16:45:23 +0530

    remove and edit feature completed.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const taskContainer = document.querySelector(".task__container");
 
-function  loadCard(card) {
+function loadCard(card) {
   const createNewCard = createCard(card);
   taskContainer.insertAdjacentHTML("beforeend", createNewCard);
   globalStore.push(card);
@@ -8,11 +8,18 @@ function  loadCard(card) {
 
 let globalStore = [];
 
-const createCard = ({ id, url, title, type, description }) => ` <div class="col-lg-3 col-md-5 mb-2" id=${id} onclick="deleteCard.apply()">
+
+const createCard = ({
+  id,
+  url,
+  title,
+  type,
+  description
+}) => ` <div class="col-lg-3 col-md-5 mb-2" id=${id} >
 <div class="card shadow">
   <div class="card-header">
-    <button class="btn btn-outline-success">
-      <i class="fas fa-edit"></i>&nbsp; Edit
+    <button class="btn btn-outline-success" onclick="editCard()">
+      <i class="fas fa-edit" onclick="editCard()"></i>&nbsp; Edit
     </button>
     <button class="btn btn-outline-danger float-end" id=${id} onclick="deleteCard.apply(this,arguments)">
       <i class="fas fa-trash-alt"></i> &nbsp; Delete
@@ -24,14 +31,14 @@ const createCard = ({ id, url, title, type, description }) => ` <div class="col-
     alt="..."
   />
   <div class="card-body">
-    <h5 class="card-title fw-bold">${title}</h5>
-    <p class="card-text">
+    <h5 class="card-title fw-bold" id="edittitle">${title}</h5>
+    <p class="card-text" id="editdesc">
      ${description}
     </p>
-    <span class="badge bg-primary">${type}</span>
+    <span class="badge bg-primary" id="edittype">${type}</span>
   </div>
   <div class="card-footer text-muted">
-    <button class="btn btn-outline-primary float-end">
+    <button class="btn btn-outline-primary float-end" id="footer-btn" onclick="saveEdit()">
       <i class="fas fa-expand-alt"></i>&nbsp; Open Task
     </button>
   </div>
@@ -40,9 +47,11 @@ const createCard = ({ id, url, title, type, description }) => ` <div class="col-
 
 const loadInitialCard = () => {
   const getCard = localStorage.getItem("tasky");
-  const { cards } = JSON.parse(getCard);
+  const {
+    cards
+  } = JSON.parse(getCard);
   cards.map((card) => {
-   loadCard(card);
+    loadCard(card);
   });
 }
 
@@ -53,21 +62,59 @@ const deleteCard = (e) => {
   const tagname = e.target.tagName;
   globalStore = globalStore.filter((obj) => obj.id !== getID);
 
-  
+
   localStorage.setItem("tasky", JSON.stringify({
     cards: globalStore
   }));
-  
-  if (tagname === "BUTTON") {
+
+  if (tagname == "BUTTON") {
     return taskContainer.removeChild(e.target.parentNode.parentNode.parentNode);
+  } else {
+
+    return taskContainer.removeChild(e.target.parentNode.parentNode.parentNode.parentNode);
+
   }
 
 
 
-    return taskContainer.removeChild(e.target.parentNode.parentNode.parentNode.parentNode);
- 
- 
 
+
+
+};
+
+const editCard = () => {
+  let editElement = [document.getElementById("editdesc"), document.getElementById("edittype"), document.getElementById("edittitle")];
+  editElement.map((element) => {
+    element.contentEditable = true;
+    element.style.border = "thin solid rgb(128, 128, 128)";
+    element.style.outlineColor = "rgb(128, 128, 128)";
+  });
+
+  const footerBtn = document.getElementById("footer-btn");
+  footerBtn.innerHTML = `<i class="fas fa-save"></i>&nbsp; Save Changes`;
+  footerBtn.className = "btn btn-primary float-end";
+};
+
+const saveEdit = () => {
+  let editElement = [document.getElementById("editdesc"), document.getElementById("edittype"), document.getElementById("edittitle")];
+
+  editElement.map((element) => {
+    element.contentEditable = "false";
+    element.style.border = "none";
+    element.style.outlineColor = "none";
+    element.value = element.textContent;
+  });
+  globalStore.map((e) => {
+    e.title = document.getElementById("edittitle").value;
+    e.type = document.getElementById("edittype").value;
+    e.description = document.getElementById("editdesc").value;
+  });
+  localStorage.setItem("tasky", JSON.stringify({
+    cards: globalStore
+  }))
+  const footerBtn = document.getElementById("footer-btn");
+  footerBtn.innerHTML = `<i class="fas fa-expand-alt"></i>&nbsp; Open Task`;
+  footerBtn.className = "btn btn-outline-primary float-end";
 }
 
 
@@ -83,6 +130,8 @@ const saveChanges = () => {
   }
 
   loadCard(taskDetails);
-  localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
+  localStorage.setItem("tasky", JSON.stringify({
+    cards: globalStore
+  }));
 
 }
